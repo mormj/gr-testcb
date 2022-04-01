9418696541cd1bb3c4bc249e85071a2736049809
Josh Morman <jmorman@gnuradio.org>
2022-04-01 09:45:00 -0400

    Add memcpy to foo block
    Signed-off-by: Josh Morman <jmorman@gnuradio.org>

--- lib/foo_impl.cc
+++ lib/foo_impl.cc
@@ -16,9 +16,7 @@
 namespace gr {
 namespace testcb {
 
-#pragma message("set the following appropriately and remove this warning")
 using input_type = float;
-#pragma message("set the following appropriately and remove this warning")
 using output_type = float;
 foo::sptr foo::make() { return gnuradio::make_block_sptr<foo_impl>(); }
 
@@ -34,10 +32,8 @@ foo_impl::foo_impl()
                      gr::io_signature::make(
                          1 /* min outputs */, 1 /*max outputs */, sizeof(output_type), buffer_double_mapped::type))
 #else
-                     gr::io_signature::make(
-                         1 /* min inputs */, 1 /* max inputs */, sizeof(input_type), host_buffer::type),
-                     gr::io_signature::make(
-                         1 /* min outputs */, 1 /*max outputs */, sizeof(output_type), host_buffer::type))
+          gr::io_signature::make(1, 1, sizeof(input_type), host_buffer::type),
+          gr::io_signature::make(1, 1, sizeof(output_type), host_buffer::type))
 #endif
 {
     set_history(100);
@@ -55,8 +51,8 @@ int foo_impl::work(int noutput_items,
     auto in = static_cast<const input_type*>(input_items[0]);
     auto out = static_cast<output_type*>(output_items[0]);
 
-           
-   // Tell runtime system how many output items we produced.
+    memcpy(out, in + history() - 1, sizeof(input_type) * noutput_items);
+    // Tell runtime system how many output items we produced.
     return noutput_items;
 }
 
